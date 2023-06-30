268d0df0732e778b7d9e92a5e3ba0cb47dcd9919
Rajiv <rajivpatkar01@gmail.com>
2023-06-30 17:00:44 +0530

    first commit

---
+++ python.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+import warnings
+import pickle
+
+warnings.filterwarnings("ignore")
+import math
+
+data=pd.read_csv('C://Users/rajiv/Desktop/FP1/Forest_fire.csv')
+data = np.array(data)
+
+X = data[1:,1:-1]
+y = data[1: , -1]
+y = y.astype('int')
+x = X.astype('int')
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.3 , random_state=0)
+log_reg = LogisticRegression()
+
+log_reg.fit(X_train , y_train)
+
+pickle.dump(log_reg,open('C://Users/rajiv/Desktop/FP1/model.pkl','wb'))
+model=pickle.load(open('C://Users/rajiv/Desktop/FP1/model.pkl','rb'))
